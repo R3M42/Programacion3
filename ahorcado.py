43a8f8b7b066b72b557e7c1a4638c1d6e201b702
R3M42 <rolandoe3m@gmail.com>
2023-08-29 18:11:03 -0400

    version 2023-08-29.02

--- ahorcado.py
+++ ahorcado.py
@@ -1,9 +1,18 @@
 def jugar():
-  print('================================')
-  print('Bienvenido al Juego del Ahorcado')
-  print('================================')
-
-  print("Fin del Juego")
+    print ("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°")
+    print ("Bienvenido al juego del ahorcado")
+    print ("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°")
+    
+    palabra_secreta = "mandarina"
+    
+    ahorcado = False
+    acerto = False
+    
+    while(not ahorcado and not acerto):
+        print("Jugando....")
+        
+        
+    print("Fin del juego")
     
 if(__name__ == "__main__"):
     jugar()
